345029e6b390fb5f94acf9ac6fde06570eb68127
sugardrunk <jannekahkonen@gmail.com>
2015-03-08 12:21:47 +0200

    fix whitespace and tabs

--- js/views/search_01.js
+++ js/views/search_01.js
@@ -1,16 +1,16 @@
 (function ()
 {
     'use strict';
-	
-	jQuery(document).ready(function ()
-	{
-		get_search_form();	
-    });	
-	
-	function get_search_form()
-	{
-		jQuery('#filter').keyup(function()
-        {	
+
+    jQuery(document).ready(function ()
+    {
+        get_search_form();
+    });
+
+    function get_search_form()
+    {
+        jQuery('#filter').keyup(function()
+        {
             // Retrieve the input field text and reset the count to zero
             var filter = jQuery(this).val(), count = 0;
 
@@ -21,28 +21,28 @@
                 if (jQuery(this).text().search(new RegExp(filter, 'i')) < 0)
                 {
                     jQuery(this).hide();
-					jQuery('.test').hide();					
-					jQuery('.main').hide();
-					jQuery('.pagination').hide();
-					
-					jQuery('#event_search_results').show();					
-                }               
-				else
-				{
-					jQuery(this).show();
-					jQuery(this).css('margin-bottom', '31px');
-				}			
+                    jQuery('.test').hide();
+                    jQuery('.main').hide();
+                    jQuery('.pagination').hide();
+
+                    jQuery('#event_search_results').show();
+                }
+                else
+                {
+                    jQuery(this).show();
+                    jQuery(this).css('margin-bottom', '31px');
+                }
             });
-			
-			if (jQuery('#filter').val() === "")
-			{	
-				jQuery('.test').show();
-				jQuery('.main').show();
-				jQuery('.pagination').show();
-				
-				jQuery('#event_search_results').hide();				
-			}
+
+            if (jQuery('#filter').val() === "")
+            {
+                jQuery('.test').show();
+                jQuery('.main').show();
+                jQuery('.pagination').show();
+
+                jQuery('#event_search_results').hide();
+            }
         });
-	}
+    }
 
 })();
